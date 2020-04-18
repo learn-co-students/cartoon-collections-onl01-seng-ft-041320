0a57051a141a2f3b1d5f8d3cfc10d9ebba027fb9
Devone Austin <devone.austin@gmail.com>
2020-04-18 20:34:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,21 @@
 require 'pry'
 
 def roll_call_dwarves(array)
-  binding.pry
-  array.collect.each_with_index { |i, index| i < array.size }
+  array.each_with_index { |i,index| puts "#{index + 1} #{i}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  annother_array = []
+  array.collect.each {|i| new_array <<  i.capitalize }
+  new_array.collect.each {|i| i + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|x|cheese_types.include?(x)}
 end
