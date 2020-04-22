652735e4659b82b5803a1956cb1ee1c202cf59ad
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-22 18:44:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name,i|
+  puts "#{i+1}. *#{name}/"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
