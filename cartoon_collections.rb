1592a2cb9f48fdee54428e632c209959a035b5a2
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-22 18:45:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,4 +20,10 @@ def long_planeteer_calls(calls)
   end
   end
 
-
+def find_the_cheese(foods)
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end
